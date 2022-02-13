3eadc4f5528559e37c99f7a99bac17873807fdda
Saugat Jonchhen <saugat.jc@nectardigit.com>
2022-02-13 23:46:20 +0545

    aa

---
+++ resources/views/frontend/index.php
@@ -0,0 +1,267 @@
+<?php include_once "header.php"; ?>
+
+<section id="site_banner" class="wow bounceInDown ">
+    <div id="site_slider_o" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images/b1.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Reconnect with your soul</h5>
+                    <a href="#" class="btn btn-secondary">Book an appoinment</a>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/b2.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block wow bounceInDown">
+                    <h5>Begin your journey towards a holistic, mindful lifestyle.</h5>
+                    <a href="#" class="btn btn-secondary">Book an appoinment</a>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/b3.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block wow bounceInDown">
+                    <h5>Release. Rebalance</h5>
+                    <a href="#" class="btn btn-secondary">Book an appoinment</a>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/b4.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block wow bounceInDown">
+                    <h5>Nourish by Avata</h5>
+                    <a href="#" class="btn btn-secondary">Book an appoinment</a>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/b5.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block wow bounceInDown">
+                    <h5>Healthy food taste better</h5>
+                    <a href="#" class="btn btn-secondary">Book an appoinment</a>
+                </div>
+            </div>
+            <div class="show_down">
+                <a href="#about_cover"> <i class="las la-angle-double-down"></i></a>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#site_slider_o" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#site_slider_o" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</section>
+<section id="about_cover" class="o_padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="about_thumbnail wow bounceInUp center">
+                    <img src="images/1631176798.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-lg-5 ms-auto">
+                <div class="about_content wow bounceInUp center" data-wow-delay="0.5s">
+                    <h4><span>About</span> Us</h4>
+                    <p>Avata was brought to conception with the vision to detox our mind and body from our hectic city
+                        life. Our instructors are here to guide you in your journey towards reconnecting with your
+                        spiritual self and help you find the perfect harmony between your mind, body, and soul. Situated
+                        in the heart of the city, we do not just provide you with spa services but with an escape from
+                        your busy life and give you a holistic wellness experience.</p>
+                    <p>your
+                        spiritual self and help you find the perfect harmony between your mind, body, and soul.
+                        Situated
+                        in t.</p>
+                    <a href="#" class="btn btn-link">Read More<i class="las la-angle-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="service_cover" class="o_buttom_padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 m-auto text-center">
+                <div class="site_title wow bounceInDown">
+                    <h3><span>Our Services</span></h3>
+                    <p><i>Take a deep breath, relax and let us take care of you. Join us for a complete mindful
+                            experience.</i></p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="owl-carousel" id="services_os_slider">
+
+                    <div>
+                        <div class="service_thumbnail wow bounceInUp">
+                            <div class="service_images">
+                                <img src="images/a1.jpg" alt="">
+                            </div>
+                            <div class="service_caption">
+                                <p>Yoga</p>
+                                <a href="#" class="btn btn-secondary">Details<i class="las la-angle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="service_thumbnail wow bounceInUp" data-wow-delay="0.5s">
+                            <div class="service_images">
+                                <img src="images/a2.jpg" alt="">
+                            </div>
+                            <div class="service_caption">
+                                <p>Cafe</p>
+                                <a href="#" class="btn btn-secondary">Details<i class="las la-angle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="service_thumbnail wow bounceInUp" data-wow-delay="0.6s">
+                            <div class="service_images">
+                                <img src="images/a3.jpg" alt="">
+                            </div>
+                            <div class="service_caption">
+                                <p>Spa</p>
+                                <a href="#" class="btn btn-secondary">Details<i class="las la-angle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="service_thumbnail wow bounceInUp" data-wow-delay="0.7s">
+                            <div class="service_images">
+                                <img src="images/a4.jpg" alt="">
+                            </div>
+                            <div class="service_caption">
+                                <p>shop</p>
+                                <a href="#" class="btn btn-secondary">Details<i class="las la-angle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="service_thumbnail wow bounceInUp" data-wow-delay="0.8s">
+                            <div class="service_images">
+                                <img src="images/a3.jpg" alt="">
+                            </div>
+                            <div class="service_caption">
+                                <p>Spa</p>
+                                <a href="#" class="btn btn-secondary">Details<i class="las la-angle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+<section id="testimonial_cover" class="o_buttom_padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 m-auto text-center">
+                <div class="site_title wow bounceInDown">
+                    <h3><span>Clients Testimonials</span></h3>
+                    <p><i>We thrive when you do; we value your experience.</i></p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="owl_slider">
+                    <div class="owl-carousel" id="testimonial_carousel">
+                        <div>
+                            <div class="testimonial_items text-center wow bounceInUp" data-wow-delay="0.5s">
+                                <div class="testimonial_images">
+                                    <img src="images/t1.jpg" alt="">
+                                </div>
+                                <h4>Palzom Pradhan</h4>
+                                <span>Nepal</span>
+                                <p>Hi, I enjoyed the first class today. Looking forward to restoring my flexibility this
+                                    month.</p>
+                                <a href="#" class="btn btn-secondary">Read More<i class="las la-angle-right"></i></a>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="testimonial_items text-center wow bounceInUp" data-wow-delay="0.7s">
+                                <div class="testimonial_images">
+                                    <img src="images/t1.jpg" alt="">
+                                </div>
+                                <h4>Palzom Pradhan</h4>
+                                <span>Nepal</span>
+                                <p>Hi, I enjoyed the first class today. Looking forward to restoring my flexibility this
+                                    month.</p>
+                                <a href="#" class="btn btn-secondary">Read More<i class="las la-angle-right"></i></a>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="testimonial_items text-center wow bounceInUp" data-wow-delay="0.9s">
+                                <div class="testimonial_images">
+                                    <img src="images/img_avatar.png" alt="">
+                                </div>
+                                <h4>Palzom Pradhan</h4>
+                                <span>Nepal</span>
+                                <p>Hi, I enjoyed the first class today. Looking forward to restoring my flexibility this
+                                    month.</p>
+                                <a href="#" class="btn btn-secondary">Read More<i class="las la-angle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="events_cover" class="o_buttom_padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 m-auto text-center">
+                <div class="site_title wow bounceInDown">
+                    <h3><span>Latest News & Events</span></h3>
+                    <p><i>Read some of our news and events</i></p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="event_thumbnail wow bounceInUp">
+                    <div class="event_thumbnail_images">
+                        <img src="images/c1.jpg" alt="">
+                    </div>
+                    <div class="event_caption">
+                        <h4>SOUND BATH - FEBRUARY, 2022</h4>
+                        <p>Come lay your mind to rest and find your center again with Ramesh.</p>
+                        <a href="#" class="btn btn-secondary">Read More<i class="las la-angle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="event_thumbnail wow bounceInUp">
+                    <div class="event_thumbnail_images">
+                        <img src="images/c2.jpg" alt="">
+                    </div>
+                    <div class="event_caption">
+                        <h4>YOGA HIKE - MARCH, 2022</h4>
+                        <p>We are so excited to announce our first Yoga & Hike class happening this March.
+                            Take a.</p>
+                        <a href="#" class="btn btn-secondary">Read More<i class="las la-angle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="event_thumbnail wow bounceInUp">
+                    <div class="event_thumbnail_images">
+                        <img src="images/c3.jpg" alt="">
+                    </div>
+                    <div class="event_caption">
+                        <h4>TRATAKA MEDITATION - APRIL, 2022</h4>
+                        <p>Trataka (or candle gazing) is a powerful and potent tech...</p>
+                        <a href="#" class="btn btn-secondary">Read More<i class="las la-angle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include_once "footer.php"; ?>
